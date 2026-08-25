b0aa8b7dcd53b14a2bf987dead874c7642f7e6b7
DarianSchruder <schreuderdb@gmail.com>
2026-08-25 19:34:08 +0200

    Added Credential Hasher

--- android/database/build.gradle.kts
+++ android/database/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 
     implementation("net.zetetic:sqlcipher-android:4.17.0@aar")
     implementation("androidx.sqlite:sqlite:2.6.2")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.78.1")
 
     testImplementation("junit:junit:4.13.2")
 
