2c90970d2a1b739f87128e5674b582bc5a64c13d
Kurtleigh <kurtleighmartin35@gmail.com>
2026-08-26 15:25:44 +0200

    Add files via upload

--- android/database/build.gradle.kts
+++ android/database/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -11,6 +13,20 @@ android {
 
     defaultConfig {
         minSdk = 23
+
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
@@ -31,5 +47,9 @@ dependencies {
 
     androidTestImplementation(
         "androidx.test:core:1.7.0"
+    )
+
+    androidTestImplementation(
+        "androidx.test:runner:1.7.0"
     )
 }
