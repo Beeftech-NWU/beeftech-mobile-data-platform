529157ecf23e980f28cd82277dbd8fcb3c56d171
MarnusLangenhoven <langenhoven123marnus@gmail.com>
2026-08-27 08:53:46 +0200

    Create build.gradle.kts

---
+++ android/authentication/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.beeftech.authentication"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+}
