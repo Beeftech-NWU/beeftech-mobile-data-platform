b6fd3a825724d1b0e916cb6009515990175a3388
BERNED1 <berned1@discovery.co.za>
2026-09-14 22:17:25 +0200

    Implement authentication foundation

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
     id("application")
 }
 
@@ -18,6 +19,13 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.5.16")
 
     testImplementation(kotlin("test"))
+
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.0.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:3.0.3")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    implementation("com.auth0:java-jwt:4.4.0")
 }
 
 tasks.test {
